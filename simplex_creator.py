5306a4b6b19a93ecccdd909c9d196f2ec86d1de0
Herbert Jiafu Wang <herbert.wang@duke.edu>
2021-11-21 17:48:15 -0600

    weighted testing:

--- simplex_creator.py
+++ simplex_creator.py
@@ -3,19 +3,22 @@ import diode
 import dionysus
 
 
-# data = np.loadtxt("County_Location.csv", delimiter=',', usecols=range(1,3))
+data = np.loadtxt("County_Location.csv", delimiter=',', usecols=range(1,3))
+lst = [1e-1] * 50
+lst2 = [1e-1] * 50
+lst0 = np.append(lst, lst2)
 
-# simplices = diode.fill_alpha_shapes(data)
+lst1 = [1] * 100
+data_3D = np.column_stack((data,lst0))
+print(data_3D)
 
-# f = dionysus.Filtration(simplices)
-# print(f)
-
-t = np.linspace(0, 2 * np.pi, 40)
-x = np.sin(t)
-y = np.sin(t) * np.cos(t)
-data = np.column_stack((x,y))
-simplices = diode.fill_alpha_shapes(data)
-
+simplices = diode.fill_alpha_shapes(data_3D)
 print(simplices)
+
+data_3D_weighted = np.column_stack((data_3D,lst1))
+print(data_3D_weighted)
+# simplices = diode.fill_weighted_alpha_shapes(data_3D)
+# print(simplices)
+
 f = dionysus.Filtration(simplices)
 print(f)
