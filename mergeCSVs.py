91e2c7b68860cb28a6d096355bff72d6f5e12e9c
kpowers2000 <82614160+kpowers2000@users.noreply.github.com>
2021-11-22 00:11:56 -0500

    Update mergeCSVs.py
    updated make_json to include weighted distributions

--- mergeCSVs.py
+++ mergeCSVs.py
@@ -4,22 +4,18 @@ import json
 
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments
-def make_json(csvFilePath, jsonFilePath):
-	return
-
-
-if __name__ == "__main__":
-    # Driver Code
-
-    # Decide the two file paths according to your
-    # computer system
-    #csvFilePath = r'Cases_And_Deaths_By_County.csv'
+def make_json():
     csvFilePath = r'covid_cases_2.csv'
     jsonFilePath = r'Cases_By_County.json'
 
     with open('Cases_And_Deaths_By_County.csv', encoding='utf-16', newline='\n') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         data_set = {}
+        total_cases = []
+        total_deaths = []
+        weighted_cases = 0
+        weighted_deaths = 0
+
         for row in spamreader:
             temp = '\t'.join(row)
             temp2 = temp.split('\t')
@@ -34,16 +30,38 @@ if __name__ == "__main__":
 
             date = temp2[0]
             county = temp2[1]
-            cases = temp2[2]
-            deaths = temp2[3]
-            
-            #create a json
-            if date not in data_set:
-                data_set[date] = {county:[int(cases), int(deaths)]}
-            elif county not in data_set[date]:
-                data_set[date][county] = [int(cases), int(deaths)]
+            cases = int(temp2[2])
+            deaths = int(temp2[3])
 
+            #create a json
+
+            if date not in data_set:
+                data_set[date] = {county:[cases, deaths, weighted_cases, weighted_deaths]}
+                total_cases.append(cases)
+                total_deaths.append(deaths)
+            elif county not in data_set[date]:
+                data_set[date][county] = [cases, deaths, weighted_cases, weighted_deaths]
+                total_cases[-1] += cases
+                total_deaths[-1] += deaths
+        mylist = data_set.keys()
+
+        weighted_avg_cases = [0]*179*100
+        weighted_avg_deaths = [0]*179*100
+        
+        i = 0
+        checker = 0
+        for date in mylist:
+            for county in data_set[date].keys():
+                case_and_death_data = data_set[date][county]
+                case_and_death_data[2] = case_and_death_data[0]/total_cases[i]
+                if total_deaths[i] == 0:
+                    continue
+                case_and_death_data[3] = case_and_death_data[1]/total_deaths[i]
+            i+=1
         with open("Cases_By_County.json", "w") as outfile:
             json.dump(data_set, outfile)
-                
+    return
 
+
+if __name__ == "__main__":
+    make_json()
